ba9e774bf0e9eb05511b6b2db642f7943a913a27
Vadym <46047631+VadymKolesov@users.noreply.github.com>
2023-12-28 18:36:26 +0100

    init

---
+++ js/bonuses.js
@@ -0,0 +1,23 @@
+export const bonuses = [
+  {
+    item: {
+      title: "Wood",
+      cost: 100,
+      power: 2,
+    },
+  },
+  {
+    item: {
+      title: "Silver",
+      cost: 100,
+      power: 2,
+    },
+  },
+  {
+    item: {
+      title: "Gold",
+      cost: 100,
+      power: 2,
+    },
+  },
+];
